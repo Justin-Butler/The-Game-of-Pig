9e01e2b8df3b15b8871c8ded13f77a3c1c7a2b05
Justin-Butler <42967802+Justin-Butler@users.noreply.github.com>
2019-02-28 14:23:16 -0500

    Create app.js
    Added my JavaScript Code to test the Game. Computer I am currently on is dated so I have been coding in CodePen. Testing Dice Roll Function.

---
+++ app.js
@@ -0,0 +1,153 @@
+/*
+GAME RULES:
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+*/
+
+/*FUNCTIONS*/
+//Updates the GUI for the Turn Change
+function turnChange() {
+  if (turnTracker === 1) {
+    playerOneName.classList.remove("active");
+    playerTwoName.classList.add("active");
+    turnTracker = 2;
+  }
+  else {
+    playerTwoName.classList.remove("active");
+    playerOneName.classList.add("active");
+    turnTracker = 1;
+  }
+}
+//Checks to see if the Game is currently won (Player Reaches a Held Score of 100)
+function gameOverChecker() {
+  if (playerOne.heldScore >= 100 || playerTwo.heldScore >= 100) {
+    gameOver = true;
+  }
+  else {
+    if (turnTracker === 1) {
+      turnChange();
+    }
+    else {
+      turnChange();
+    }
+  }
+}
+//Randomizes a number from 1-6 as the die is six sided. Performs Animation and then displays the result to the player and returns the value of the die.
+function rollDice() {
+  var result = Math.round(Math.random() * 7);
+  var diceImg = 1;
+  var anim = setInterval(frame, 200);
+  function frame() {
+    if (diceImg > 6){
+      clearInterval(anim);
+    }
+    else {
+      dice.src = "dice/dice-" + diceImg + ".png";
+      diceImg++;
+    }
+  }
+  dice.src = "dice/dice-" + result + ".png";
+  return result;
+}
+//Calls Dice Roll Function and uses the result to update the score of the player.
+function answerCheck() {
+  if (!gameOver){
+    var roll = rollDice();
+    //If a 1 is rolled
+    if (roll === 1) {
+      if (turnTracker === 1) {
+        playerOne.tempScore = 0;
+        playerOneTempScore.innerHTML = playerOne.tempScore;
+        turnChange();
+      }
+      else {
+        playerTwo.tempScore = 0;
+        playerTwoTempScore.innerHTML = playerTwo.tempScore;
+        turnChange();
+      }
+    }
+    //If a number above 1 is rolled
+    else {
+      if (turnTracker === 1) {
+        playerOne.tempScore = this + roll;
+        playerOneTempScore.innerHTML = playerOne.tempScore;
+      }
+      else {
+        playerTwo.tempScore = this + roll;
+        playerTwoTempScore.innerHTML = playerTwo.tempScore;
+      }
+    }
+  } 
+}
+//Adds the Value of the Temp Score to the Held Score and checks if the game is won.
+function holdScore() {
+  if (!gameOver) {
+    if (turnTracker === 1) {
+      playerOne.heldScore = this + playerOne.tempScore;
+      playerOne.tempScore = 0;
+      playerOneTempScore.innerHTML = playerOne.tempScore;
+      playerOneHeldScore.innerHTML = playerOne.heldScore;
+    }
+    else {
+      playerTwo.heldScore = this + playerTwo.tempScore;
+      playerTwo.tempScore = 0;
+      playerTwoTempScore.innerHTML = playerTwo.tempScore;
+      playerTwoHeldScore.innerHTML = playerTwo.heldScore;
+    }
+    gameOverCheck();
+  }
+}
+//New Game Function
+function newGame() {
+  playerOne.tempScore = 0;
+  playerOne.heldScore = 0;
+  playerTwo.tempScore = 0;
+  playerTwo.tempScore = 0;
+  playerOneTempScore.innerHTML = playerOne.tempScore;
+  playerOneHeldScore.innerHTML = playerOne.heldScore;
+  playerTwoTempScore.innerHTML = playerTwo.tempScore;
+  playerTwoHeldScore.innerHTML = playerTwo.heldScore;
+  gameOver = false;
+  if (turnTracker === 2) {
+    turnChange();
+  }
+}
+
+/*GLOBAL VARIABLES*/
+
+//Player Scores
+var playerOne = {
+  tempScore: 0,
+  heldScore: 0
+};
+var playerTwo = {
+  tempScore: 0,
+  heldScore: 0
+};
+
+//DOM Variables
+var playerOneName = document.getElementsByClassName("player-0-panel");
+var playerTwoName = document.getElementsByClassName("player-1-panel");
+var playerOneHeldScore = document.getElementById("score-0");
+var playerOneTempScore = document.getElementById("current-0");
+var playerTwoHeldScore = document.getElementById("score-1");
+var playerTwoTempScore = document.getElementById("current-1");
+var dice = document.getElementsByClassName("dice");
+var newGameBtn = document.getElementsByClassName("btn-new");
+var rollDiceBtn = document.getElementsByClassName("btn-roll");
+var holdBtn = document.getElementsByClassName("btn-hold");
+
+//Other Global Variables 
+var turnTracker = 1;
+var gameOver = false;
+
+/*INITIATE*/
+newGame();
+
+/*Event Listeners*/
+newGameBtn.addEventListener("click", newGame);
+rollDiceBtn.addEventListener("click", answerCheck);
+holdBtn.addEventListener("click", holdScore);
